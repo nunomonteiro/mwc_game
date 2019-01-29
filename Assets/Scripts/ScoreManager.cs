f476491d4842dfc6067e6a4188adf6bd68577644
Nuno Monteiro <nuno.monteir@gmail.com>
2019-01-29 13:05:00 +0000

    Created ScoreManager to store and push results to google spreadsheet
    Added helper class that sends data to google spreadsheet

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+
+    private SendToGoogle _sendToGoogle;
+
+	// Use this for initialization
+	void Awake () {
+        _sendToGoogle = GetComponent<SendToGoogle>();
+	}
+	
+    public void PostNewScore(string name, string mail, int score, bool wantsMore) {
+        _sendToGoogle.Send(name, mail, wantsMore ? "YES" : "NO", score.ToString());
+
+        //TODO add score to local list to showcase top winners
+        //TODO if the TV has no internet this has to write to a local file
+    }
+}
