85acb8f0177775d3c4112813aa0b9106dd2a8cb7
Nuno Monteiro <nuno.monteir@gmail.com>
2019-01-29 11:54:36 +0000

    Added support for fetching info from google spreadsheets
    Info is fetched and handled properly in a test scene to showcase advantages

---
+++ Assets/Scripts/Advantage.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Advantage : MonoBehaviour
+{
+
+    [SerializeField]
+    private Text _title;
+
+    [SerializeField]
+    private Text _text;
+
+    [SerializeField]
+    private Image _image;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Init(string title, string text, string imgURL)
+    {
+        _title.text = title;
+        _text.text = text;
+        WWWImageFetcher.FillImageFromURL(_image, imgURL);
+    }
+}
