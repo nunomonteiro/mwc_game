191bbbbe9030ce4c6f81cb4f4ef080c43e248360
Mikie Ribeiro <mikie.ribeiro@aptoide.com>
2019-02-01 10:17:07 +0000

    APPC-900
    -Script RingController added
    -OnTriggerEnter2D method created

---
+++ Assets/RingController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingController : MonoBehaviour {
+
+    // when the GameObjects collider arrange for this GameObject to travel to the left of the screen
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log(col.name);
+    }
+}
