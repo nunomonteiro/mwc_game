30e3c903e7239ed22f2454510b0fd69e66285519
Bruno Varela <bruno.varela@aptoide.com>
2019-02-01 10:10:26 +0000

    Catapult Mecanics
    Classes and images for the catapult Mechanics

---
+++ Assets/Scripts/Catapult Scripts/PullAndRelease.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PullAndRelease : MonoBehaviour {
+
+    // The default Position
+    Vector2 startPos;
+    float radius;
+    public float force = 0.01f;
+
+    private Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        startPos = transform.position;
+        anim = GameObject.FindGameObjectWithTag("Arm").GetComponent<Animator>();    
+    }
+
+    void OnMouseUp()
+    {
+        anim.SetTrigger("shoot");
+        GetComponent<Rigidbody2D>().isKinematic = false;
+        Vector2 dir = startPos - (Vector2)transform.position;
+        GetComponent<Rigidbody2D>().AddForce(dir * force);
+        Destroy(this);
+    }
+
+
+
+
+    void OnMouseDrag()
+    {
+        // Convert mouse position to world position
+        Vector2 p = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // Keep it in a certain radius
+        float radius = 0.5f;
+        Vector2 dir = p - startPos;
+        if (dir.sqrMagnitude > radius)
+            dir = dir.normalized * radius;
+
+        // Set the Position
+        transform.position = startPos + dir;
+    }
+}
---
+++ Assets/Scripts/Catapult Scripts/Spawn.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+    public GameObject birdPrefab;
+
+    public GameObject spawnPosition;
+  
+    private void Start()
+    {
+        spawnNext();
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    void spawnNext()
+    {
+        Instantiate(birdPrefab, transform.position, Quaternion.identity);
+    }
+
+    void OnTriggerExit2D(Collider2D co)
+    {
+
+    }
+
+    bool sceneMoving()
+    {
+        // Find all Rigidbodies, see if any is still moving a lot
+        Rigidbody2D[] bodies = FindObjectsOfType(typeof(Rigidbody2D)) as Rigidbody2D[];
+        foreach (Rigidbody2D rb in bodies)
+            if (rb.velocity.sqrMagnitude > 5)
+                return true;
+        return false;
+    }
+}
