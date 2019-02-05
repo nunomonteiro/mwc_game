f41e347403fb2ed76dc3f74074e0b11a38218fbe
Nuno Monteiro <nuno.monteir@gmail.com>
2019-02-05 09:52:19 +0000

    Added rewards controller

---
+++ Assets/Scripts/RewardsController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RewardsController : MonoBehaviour {
+
+    public bool caughtRing1;
+    public bool caughtRing2;
+    public bool caughtRing3;
+    public int timeLeft;
+}
